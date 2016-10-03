67fba15ffe2099292901c6965fca732d68085d05
Alex820202 <ads2004@bk.ru>
2016-10-03 17:58:50 +0700

    Остановился на определении текущей страницы.

--- lesson_01_guestBook/stage_02/index.php
+++ lesson_01_guestBook/stage_02/index.php
@@ -1,3 +1,82 @@
+<?php
+require_once(__DIR__.'/config.php');
+
+$post_per_page = 5; // количество записей на странице
+$number_visible_pages = 5;
+
+try{
+	$dsn = "mysql:host=$host;dbname=$dbname";
+	$dbh = new PDO($dsn, $user, $password);
+	$dbh->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+	$dbh->setAttribute( PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+	
+	$query_total= 'SELECT COUNT(*) FROM posts';
+	$query_select = 'SELECT * FROM posts LIMIT start = :start, $number = :number';
+	$query_insert = 'INSERT INTO posts(author, datetime, text) VALUES (:author_name, :datetime, :text_post)';
+	
+	$sth_total = $dbh->prepare($query_total);
+	$sth_select = $dbh->prepare($query_select);
+	$sth_insert = $dbh->prepare($query_insert);
+	
+	$sth_total->execute();
+	$result_total = $sth_total->fetch();
+	$total = $result_total['COUNT(*)']; // общее количество постов в гостевой книге
+	$total_page = ceil($total/$post_per_page);//максимально возможное количество страниц
+	/*
+	* Определяем текущую страницу вывода сообщений.
+	*/
+	if(!$_GET['page'] && !$_POST){
+		$current_page = 1;
+	}else{
+		$current_page = $_GET['page'];
+	}
+	/*
+	* Определяем массив выводимых в пагинации страниц
+	*/
+	if($current_page-2<0 && $current_page+2>=$total_page){
+		$start_page = 1;
+		$end_page = $total_page;
+	}elseif($current_page-2>=0 && $current_page+2>=$total_page){
+		$start_page = $current_page-2;
+		$end_page = $total_page;
+	}elseif($current_page-2>=0 && $current_page+2<$total_page){
+		$start_page = $current_page-2;
+		$end_page = $current_page+2;
+	}else{
+		$start_page = 1;
+		$end_page = $current_page+2;
+	}
+	
+	for($i=$start_page; $i<$end_page+1; $i++){
+		$page_pagination[] = $i;
+	}
+		/*
+	* Определяем начальный пост и число постов на странице. 
+	*/
+	if(!$_GET['page'] || $_GET['page'] == 1){//первая страница
+		$start = 1; // с какого поста начинаем вывод
+		$number = $total; // сколько постов показываем
+	}elseif($total_page == $_GET['page']){//последняя страница
+		$start = $post_per_page*$_GET['page']-$post_per_page;
+		$number = $total - $start;
+	}else{//любая другая страница
+		$start = $_GET['page']*$post_per_page - $post_per_page;
+		$number = $post_per_page;}
+	/*
+	* 
+	*/
+	
+}catch(PDOException $e){
+	echo 'Хьюстон - у нас проблема!!!'.'<br>';
+	echo $e->getMessage();
+}
+?>
+
+
+
+
+
+
 <!DOCTYPE html>
 <html lang="ru">
 	<head>
