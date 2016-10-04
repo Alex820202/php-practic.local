32369578b56fae8547c501f97ac336470e15085f
Alex820202 <ads2004@bk.ru>
2016-10-04 17:25:48 +0700

    Signed-off-by: Alex820202 <ads2004@bk.ru>

---
+++ lesson_01_guestBook/stage_02/config.php
@@ -0,0 +1,6 @@
+<?php
+$host = 'localhost';
+$dbname = 'guestbook';
+$user = 'root';
+$password = '';
+?>
--- lesson_01_guestBook/stage_02/index.php
+++ lesson_01_guestBook/stage_02/index.php
@@ -9,37 +9,42 @@ try{
 	$dbh = new PDO($dsn, $user, $password);
 	$dbh->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
 	$dbh->setAttribute( PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-	
+	/*
+	*Блок запросов, которые нам нужны в работе.
+	*/
 	$query_total= 'SELECT COUNT(*) FROM posts';
-	$query_select = 'SELECT * FROM posts LIMIT start = :start, $number = :number';
+	$query_select = 'SELECT * FROM posts LIMIT :start, :number';
 	$query_insert = 'INSERT INTO posts(author, datetime, text) VALUES (:author_name, :datetime, :text_post)';
-	
+	/*
+	*Определяем общее количество постов в базе и соответственно максимальное количество страниц.
+	*/
 	$sth_total = $dbh->prepare($query_total);
-	$sth_select = $dbh->prepare($query_select);
-	$sth_insert = $dbh->prepare($query_insert);
-	
 	$sth_total->execute();
 	$result_total = $sth_total->fetch();
 	$total = $result_total['COUNT(*)']; // общее количество постов в гостевой книге
-	$total_page = ceil($total/$post_per_page);//максимально возможное количество страниц
+	if($total < $post_per_page){
+		$total_page = 1;
+	}else{$total_page = ceil($total/$post_per_page);}//максимально возможное количество страниц
+	
 	/*
 	* Определяем текущую страницу вывода сообщений.
 	*/
-	if(!$_GET['page'] && !$_POST){
-		$current_page = 1;
+	
+	if(!$_GET['page']){
+		$current_page = $total_page;
 	}else{
 		$current_page = $_GET['page'];
 	}
 	/*
 	* Определяем массив выводимых в пагинации страниц
 	*/
-	if($current_page-2<0 && $current_page+2>=$total_page){
+	if($current_page-2<=0 && $current_page+2>=$total_page){
 		$start_page = 1;
 		$end_page = $total_page;
-	}elseif($current_page-2>=0 && $current_page+2>=$total_page){
+	}elseif($current_page-2>0 && $current_page+2>=$total_page){
 		$start_page = $current_page-2;
 		$end_page = $total_page;
-	}elseif($current_page-2>=0 && $current_page+2<$total_page){
+	}elseif($current_page-2>0 && $current_page+2<$total_page){
 		$start_page = $current_page-2;
 		$end_page = $current_page+2;
 	}else{
@@ -50,27 +55,43 @@ try{
 	for($i=$start_page; $i<$end_page+1; $i++){
 		$page_pagination[] = $i;
 	}
-		/*
+	
+	/*
 	* Определяем начальный пост и число постов на странице. 
 	*/
-	if(!$_GET['page'] || $_GET['page'] == 1){//первая страница
-		$start = 1; // с какого поста начинаем вывод
-		$number = $total; // сколько постов показываем
+	if($_GET['page'] == 1){//первая страница
+		$start = 0; // с какого поста начинаем вывод
+		$number = $post_per_page; // сколько постов показываем
 	}elseif($total_page == $_GET['page']){//последняя страница
 		$start = $post_per_page*$_GET['page']-$post_per_page;
 		$number = $total - $start;
+	}elseif(!$_GET){
+		$start = $post_per_page*$total_page - $post_per_page;
+		$number = $post_per_page;
 	}else{//любая другая страница
 		$start = $_GET['page']*$post_per_page - $post_per_page;
 		$number = $post_per_page;}
-	/*
-	* 
-	*/
 	
-}catch(PDOException $e){
-	echo 'Хьюстон - у нас проблема!!!'.'<br>';
-	echo $e->getMessage();
-}
-?>
+	/*
+	*Обрабатываем сохраненное сообщение
+	*/
+	if(!empty($_POST['author']) && !empty($_POST['text'])){
+		$data_insert['author_name'] = trim($_POST['author']);
+		$data_insert['datetime'] = time();
+		$data_insert['text_post'] = htmlspecialchars(trim($_POST['text']));
+		$sth_insert = $dbh->prepare($query_insert);
+		$sth_insert->execute($data_insert);
+		header('Location: index.php?page='.$total_page, true, 303);
+	}else{
+		$sth_select = $dbh->prepare($query_select);
+		$sth_select->bindValue(start, $start, PDO::PARAM_INT);
+		$sth_select->bindValue(number, $number, PDO::PARAM_INT);
+		
+		$sth_select->execute();
+		$results = $sth_select->fetchAll();
+	}
+	echo $current_page;
+	?>
 
 
 
@@ -90,116 +111,117 @@ try{
 		
 		<div id="wrapper">
 			<h1>Гостевая книга</h1>
-			
+<!-----------------------Начало вывода верхнего блока пагинации----------------------------------------->			
 			<div>
 				<nav>
 				  <ul class="pagination">
-					<li class="disabled">
+					<?php
+					if(1 != $current_page){
+					?>	
+					<li>
 					  <a href="?page=1"  aria-label="Previous">
 						<span aria-hidden="true">&laquo;</span>
 					  </a>
-					</li>
-					<li class="active"><a href="?page=1">1</a></li>
-					<li><a href="?page=2">2</a></li>
-					<li><a href="?page=3">3</a></li>
-					<li><a href="?page=4">4</a></li>
-					<li><a href="?page=5">5</a></li>
+					 </li>
+					 <?php } 
+					 foreach($page_pagination as $page_pag){
+					 	if($current_page == $page_pag){
+					 		$class_start = "class='active'";
+					 		}else{
+							$class_start = '';
+						}
+					 	echo "<li ".$class_start." ><a href=".$_SERVER['PHP_SELF']."?page=".$page_pag.">".$page_pag."</a></li>";
+					 }
+					 ?>
+					
 					<li>
-					  <a href="?page=5" aria-label="Next">
+					 <?php if($total_page != $current_page){ ?><a href="?page=<?php echo $total_page; ?>" aria-label="Next">
 						<span aria-hidden="true">&raquo;</span>
 					  </a>
-					</li>
+					</li><?php } ?>
+				  </ul>
+				</nav>
+				
+			</div>
+<!---------------------Окончание вывода верхнего блока пагинации----------------------------------------->
+
+<!-----------------------Блок с записями гостевой книги----------------------------------------->
+			
+			<?php
+			foreach($results as $result){
+				?>
+				<div class="note">
+				<p>
+					<span class="date"><?php  echo date('H:i:s d.m.Y', $result['datetime']); 
+					?></span>
+					<span class="name"><?php echo $result['author'];
+					?></span>
+				</p>
+				<p>
+				<?php echo $result['text'];
+				?>
+				</p>
+				
+			</div>
+			<?php } ?>
+<!---------------------Окончание блока с записями гостевой книги----------------------------------------->
+			
+			
+	<!----------------Выводим пагинацию страниц внизу списка постов----------------------------->
+			<div>
+				<nav>
+				  <ul class="pagination">
+					<?php
+					if(1 != $current_page){
+					?>	
+					<li>
+					  <a href="?page=1"  aria-label="Previous">
+						<span aria-hidden="true">&laquo;</span>
+					  </a>
+					 </li>
+					 <?php } 
+					  foreach($page_pagination as $page_pag){
+					 	if($current_page == $page_pag){
+					 		$class_start = "class='active'";
+					 		}else{
+							$class_start = '';
+						}
+					 	echo "<li ".$class_start." ><a href=".$_SERVER['PHP_SELF']."?page=".$page_pag.">".$page_pag."</a></li>";
+					 }
+					 ?>
+					
+					 <?php if($total_page != $current_page){ ?>
+					 <li>
+					<a href="?page=<?php echo $total_page; ?>" aria-label="Next">
+						<span aria-hidden="true">&raquo;</span>
+					  </a>
+					</li><?php } ?>
 				  </ul>
 				</nav>
 				
 			</div>
 			
+<!-----------------------Закончили вывод пагинации---------------------------------------------->
+
+<!------------В случае добавления записи выводим уведомление "Запись успешно сохранена!"---------------->
 			
-			<div class="note">
-				<p>
-					<span class="date">18.04.2014 23:59:59</span>
-					<span class="name">Дмитрий</span>
-					
-				</p>
-				<p>
-					Lorem ipsum dolor sit amet, 
-					consectetur adipiscing elit. 
-					Nulla efficitur elementum lorem id venenatis. 
-					Nullam id sagittis urna, eu ultrices risus. 
-					Duis ante lorem, semper nec fringilla eu,
-					commodo vel mauris. Nunc tristique odio lectus, eget condimentum nunc consectetur eu. Nullam non varius nisl, aliquet fringilla lectus. Aliquam erat volutpat. Ut vel mi et lectus hendrerit ornare vel ut neque. Quisque venenatis nisl eu mi
-				</p>
-				
-			</div>	
-			<div class="note">
-				<p>
-					<span class="date">16.04.2014 14:59:59</span>
-					<span class="name">Николай</span>
-					
-				</p>
-				<p>
-					Ut varius commodo fringilla. Nullam id pulvinar odio. Pellentesque gravida aliquam ipsum, et malesuada neque molestie eget. Vestibulum sagittis finibus efficitur. Donec sit amet aliquet dolor, vitae ornare tortor. Etiam eget augue nec diam vehicula bibendum. Nulla quis erat lacus. Vestibulum quis mattis augue. Praesent dignissim, justo non aliquam feugiat, lorem metus egestas leo, quis eleifend odio quam in ex. Aenean diam est, scelerisque ac ultricies sit amet, vulputate in tortor. Etiam ac mi enim. Sed pellentesque elementum erat eu eleifend. Integer imperdiet sem eu magna feugiat, sed efficitur velit convallis. 
-				</p>
-				
-			</div>	
-			<div class="note">
-				<p>
-					<span class="date">15.04.2014 12:59:59</span>
-					<span class="name">Петр</span>
-					
-				</p>
-				<p>
-					Phasellus gravida fermentum pellentesque. Aenean non neque mollis nisl dapibus eleifend. Sed interdum dui nec dictum elementum. Proin eget semper dolor, ut commodo nibh. 
-					Quisque vitae pharetra ligula. Sed dictum, sem sed pellentesque aliquam, tellus sapien dapibus magna, eu suscipit lacus augue sed velit. Ut vehicula sagittis nulla, et aliquet elit. Quisque tincidunt sem nibh, finibus dictum nisl vulputate quis. In vitae nisl et lacus pulvinar ornare id ac libero. Morbi pharetra fringilla erat ut lacinia. 
-					
-				</p>
-				
-			</div>	
+			<?php
+			$flag = "http://".$_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI'];
 			
-			<div>
-				<nav>
-				  <ul class="pagination">
-					<li class="disabled">
-					  <a href="?page=1"  aria-label="Previous">
-						<span aria-hidden="true">&laquo;</span>
-					  </a>
-					</li>
-					<li class="active"><a href="?page=1">1</a></li>
-					<li><a href="?page=2">2</a></li>
-					<li><a href="?page=3">3</a></li>
-					<li><a href="?page=4">4</a></li>
-					<li><a href="?page=5">5</a></li>
-					<li>
-					  <a href="?page=5" aria-label="Next">
-						<span aria-hidden="true">&raquo;</span>
-					  </a>
-					</li>
-				  </ul>
-				</nav>
-				
-			</div>
-			
-			
-			<div class="info alert alert-info">
+			if($_SERVER['HTTP_REFERER']== $flag){?> <div class="info alert alert-info">
 				Запись успешно сохранена!
 			</div>
-			
-			<!--
-			
-				Обеспечьте удаление концевых пробелов
-				и тегов при сохранении.
-				
-				Обеспечьте редирект с помощью
-				header location чтобы не было 
-				двойного сохранения при обновлении страницы.
-			
-			-->
+
+
+			<?php }?>
+<!-----------------------Окончание вывода---------------------------------------------->
+<!-----------------------Форма добавления записи---------------------------------------------->
 			
 			<div id="form">
-				<form action="#form" method="POST">
-					<p><input class="form-control" placeholder="Ваше имя"></p>
+				<form action="index.php" method="POST">
+					<p><input class="form-control" placeholder="Ваше имя" name="author"></p>
 					
-					<p><textarea class="form-control" placeholder="Ваш отзыв"></textarea></p>
+					<p><textarea class="form-control" placeholder="Ваш отзыв"name="text"></textarea></p>
 					<p><input type="submit" class="btn btn-info btn-block" value="Сохранить"></p>
 				</form>
 			</div>
@@ -207,6 +229,14 @@ try{
 
 	</body>
 </html>
+		
 
+
+<?php	
+}catch(PDOException $e){
+	echo 'Хьюстон - у нас проблема!!!'.'<br>';
+	echo $e->getMessage();
+}
+?>
 
 			
