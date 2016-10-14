3eb5dbc27c3648e3e368a81c030bc274674dd358
Alex820202 <ads2004@bk.ru>
2016-10-14 22:44:29 +0700

    добавил config.php

---
+++ lesson_02_notebook/stage_02/config.php
@@ -0,0 +1,6 @@
+<?php
+$host = 'localhost';
+$dbname = 'notebook';
+$user = 'root';
+$password = '';
+?>
---
+++ lesson_04_forum/config.php
@@ -0,0 +1,6 @@
+<?php
+$host = 'localhost';
+$dbname = 'forum';
+$user = 'root';
+$password = '';
+?>
