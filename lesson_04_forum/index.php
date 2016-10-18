420582a65c5feeb321ade73e19c2b54156dd9eaf
Alex820202 <ads2004@bk.ru>
2016-10-18 15:47:59 +0700

    Задание lesson-04 готово к проверке

--- lesson_04_forum/index.php
+++ lesson_04_forum/index.php
@@ -8,38 +8,26 @@ try{
 	* Обрабатываем создание новой темы на форуме.
 	*/
 	if(!empty($_POST)){
-		$dbh -> beginTransaction();
 		$sql_inspection = 'SELECT * FROM themes WHERE themes_title=:themes_title';
-		$sql_insert_themes = 'INSERT INTO themes(themes_title, data_creation, author, number_replies) VALUES (:themes_title, :data_creation, :author, :number_replies)';
-		$sql_insert_posts = 'INSERT INTO posts(themes_number, data_post, author, text) VALUES (:themes_number, :data_post, :author, :text)';
-		$stm_insert_themes = $dbh -> prepare($sql_insert_themes);
-		$stm_insert_posts = $dbh -> prepare($sql_insert_posts);
 		$stm_inspection = $dbh -> prepare($sql_inspection);
 		$data_inspection['themes_title'] = strip_tags(trim($_POST['theme_title']));
 		$stm_inspection ->execute($data_inspection);
-		$opt0 = $stm_inspection -> fetch();	
-		if($opt0){
-			header("Location:index.php?page=".(int)$_GET['page']."&flag=1", TRUE, 303);
+		$opt0 = $stm_inspection -> fetch();
+		if(!empty($opt0)){
+			header("Location: index.php?page=".(int)$_GET['page']."&flag=1", TRUE, 303);
+		}else {
+
+			$sql_insert_themes = 'INSERT INTO themes(themes_title, themes_text, data_creation, author, number_replies) VALUES (:themes_title, :themes_text, :data_creation, :author, :number_replies)';
+			$stm_insert_themes = $dbh->prepare($sql_insert_themes);
+			$data_theme['themes_title'] = strip_tags(trim($_POST['theme_title']));
+			$data_theme['themes_text'] = strip_tags(trim($_POST['text']));
+			$data_theme['data_creation'] = time();
+			$data_theme['author'] = strip_tags(trim($_POST['author']));
+			$data_theme['number_replies'] = 0;
+			$stm_insert_themes->execute($data_theme);
+			header("Location: index.php?page=" . (int)$_GET['page'] . "&flag=2", TRUE, 303);
 		}
-		$data_theme['themes_title'] = strip_tags(trim($_POST['theme_title']));
-		$data_theme['data_creation'] = time();
-		$data_theme['author'] = strip_tags(trim($_POST['author']));
-		$data_theme['number_replies'] = 0;
-		$opt1 = $stm_insert_themes -> execute($data_theme);
-		$data_posts['themes_number'] = $dbh -> lastInsertId();
-		$data_posts['data_post'] = $data_theme['data_creation'];
-		$data_posts['author'] = strip_tags(trim($_POST['author']));
-		$data_posts['text'] = strip_tags(trim($_POST['text']));
-		$opt2 = $stm_insert_posts -> execute($data_posts);
-		if(!$opt1 || !$opt2){
-			
-			$dbh -> rollBack();
-		}else{
-			
-			$dbh -> commit();
-			header("Location: index.php?page=".(int)$_GET['page']."&flag=2", TRUE, 303);
-		}
-		
+
 	}
 	/*
 	* Определяем количество тем в базе и соответственно общее количество страниц
@@ -118,7 +106,7 @@ try{
 			?>
 			<div class="note">
 				<p class="topic">
-					<a href="topic.php?topic=3&page=1"><?php echo $result['themes_title'];?></a>
+					<a href="topic.php?topic=<?php echo $result['id'];?>&page=1"><?php echo $result['themes_title'];?></a>
 				</p>
 				<p>
 					<span class="subheader">Создана:</span>
@@ -187,132 +175,5 @@ try{
 
 ?>
 
-<!--<!DOCTYPE html>
-<html lang="ru">
-	<head>
-		<meta charset="utf-8">  
-		<title>Форум</title>
-		<link rel="stylesheet" href="bootstrap3/css/bootstrap.css">
-		<link rel="stylesheet" href="css/styles.css">
-		<link rel="stylesheet" href="css/admin.css">
-	</head>
-	<body>
-		
-		<div id="wrapper">
-			<h1>Наш форум</h1>
-			
-			
-			<div class="note">
-				Наш супер крутой форум посвящен phasellus gravida fermentum pellentesque. Aenean non neque mollis nisl dapibus eleifend. Sed interdum dui nec dictum elementum. Proin eget semper dolor, ut commodo nibh. 
-				Quisque vitae pharetra ligula. Sed dictum, sem sed pellentesque aliquam, tellus sapien dapibus magna, eu suscipit lacus augue sed velit. Ut vehicula sagittis nulla, et aliquet elit. Quisque tincidunt sem nibh, finibus dictum nisl vulputate quis. In vitae nisl et lacus pulvinar ornare id ac libero. Morbi pharetra fringilla erat ut lacinia. 
-			</div>	
-			<h2>Темы форума</h2>
-			
-			<div class="note">
-				<p class="topic">
-					<a href="topic.php?topic=3&page=1">Помогите решить проблему с HTML</a>
-				</p>
-				<p>
-					<span class="subheader">Создана:</span> 
-					18.04.2014 23:59:59.
-					<span class="subheader">Автор:</span> 
-					Дмитрий.
-					<br>
-					<span class="subheader">Количество ответов:</span> 
-					7
-				</p>
-				
-				
-			</div>	
-			<div class="note">
-				<p class="topic">
-					<a href="topic.php?topic=2&page=1">Помогите решить проблему с CSS</a>
-				</p>
-				<p>
-					<span class="subheader">Создана:</span> 
-					19.04.2014 14:59:59.
-					<span class="subheader">Автор:</span> 
-					Петр.
-					<br>
-					<span class="subheader">Количество ответов:</span> 
-					5
-				</p>
-				
-				
-			</div>		
-			<div class="note">
-				<p class="topic">
-					<a href="topic.php?topic=1&page=1">Ничего не работает! Что делать?!</a>
-				</p>
-				<p>
-					<span class="subheader">Создана:</span> 
-					19.04.2014 14:59:59.
-					<span class="subheader">Автор:</span> 
-					Иммануил.
-					<br>
-					<span class="subheader">Количество ответов:</span> 
-					53
-				</p>
-				
-				
-			</div>	
-			
-			<div>
-				<nav>
-				  <ul class="pagination">
-					<li class="disabled">
-					  <a href="?page=1"  aria-label="Previous">
-						<span aria-hidden="true">&laquo;</span>
-					  </a>
-					</li>
-					<li class="active"><a href="?page=1">1</a></li>
-					<li><a href="?page=2">2</a></li>
-					<li><a href="?page=3">3</a></li>
-					<li><a href="?page=4">4</a></li>
-					<li><a href="?page=5">5</a></li>
-					<li>
-					  <a href="?page=5" aria-label="Next">
-						<span aria-hidden="true">&raquo;</span>
-					  </a>
-					</li>
-				  </ul>
-				</nav>
-				
-			</div>
-			
-			<h2>Создать тему</h2>
-			
-			<div class="info alert alert-info">
-				Тема успешно создана!
-			</div>
-			<div class="info alert alert-danger">
-				Тема с таким назанием уже существует!
-			</div>-->
-			<!--
-			
-				Обеспечьте удаление концевых пробелов
-				и тегов при сохранении.
-				
-				Обеспечьте редирект с помощью
-				header location чтобы не было 
-				двойного сохранения при обновлении страницы.
-			
-			-->
-			
-		<!--	<div id="form">
-				<form action="#form" method="POST">
-					<p><input class="form-control" placeholder="Ваше имя"></p>
-					<p><input class="form-control" placeholder="Название темы"></p>
-					
-					<p><textarea class="form-control" placeholder="Описание темы"></textarea></p>
-					<p><input type="submit" class="btn btn-info btn-block" value="Сохранить"></p>
-				</form>
-			</div>
-			
-		</div>
-
-	</body>
-</html>
--->
 
 			
--- lesson_04_forum/topic.php
+++ lesson_04_forum/topic.php
@@ -1,7 +1,185 @@
 <?php
-echo time();
-?>
+require_once(__DIR__.'/config.php');
+$dbh = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
+$dbh -> setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+$dbh -> setAttribute( PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+$id = (int)$_GET['topic'];
+try{
+	if(!empty($_POST)){
+		$dbh -> beginTransaction();
+		$sql_select_number_post = 'SELECT number_replies FROM themes WHERE id=:id';
+		$stm_select_number_post = $dbh -> prepare($sql_select_number_post);
+		$stm_select_number_post -> execute(array('id'=>$id));
+		$result_number_replies = $stm_select_number_post -> fetch();
+		$number_replies = (int)$result_number_replies['number_replies'];
+
+		$sql_update_themes = 'UPDATE themes SET number_replies=:number_replies WHERE id=:id';
+		$stm_update_themes = $dbh -> prepare($sql_update_themes);
+		$opt = $stm_update_themes -> execute(array('number_replies'=>$number_replies+1, 'id'=>$id));
+		
+		$sql_insert_post = 'INSERT INTO posts(themes_number, data_post, author, text) VALUES (:themes_number, :data_post, :author, :text)';
+		$stm_insert_post = $dbh -> prepare($sql_insert_post);
+		$data['themes_number'] = $id;
+		$data['data_post'] = time();
+		$data['author'] = strip_tags(trim($_POST['author']));
+		$data['text'] = strip_tags(trim($_POST['text']));
+		$stm_insert_post -> execute($data); 
+		$dbh -> commit();
+	}
+} catch (PDOException $f) {
+	$dbh -> rollBack();
+	echo 'Ошибка записи в базу данных: '.$f->getMessage();
+}
+try{
+	
+	if(!empty($_GET['page'])){
+		$current_page = (int)$_GET['page'];
+		}else{
+			$current_page = 1;
+			}
+	$post_to_page = 5; // количество ответов на страницу
+	$dbh -> beginTransaction();
+	/*
+	* Вычисляем общее количество ответов в теме для задания пагинации
+	*/
+	$sql_select_post_total = 'SELECT COUNT(*) FROM posts WHERE themes_number=:id_theme';
+	$stm_select_post_total = $dbh -> prepare($sql_select_post_total);
+	$stm_select_post_total -> execute(array('id_theme'=>$id));
+	$result_total = $stm_select_post_total -> fetch();
+	if($result_total['COUNT(*)'] !=0){
+			$total_post = $result_total['COUNT(*)'];
+		}else{
+			$total_post = 1;
+		}
+	$total_page_post = ceil($total_post/$post_to_page);
+	/*
+	 * Формируем массив страниц пагинации
+	 */
+	 if($current_page-2>=0 && $current_page+2<=$total_page_post){
+	 	for($i=$current_page-2;$i<=$current_page+2;$i++){
+			$page_pagination_post[]=$i;
+		}
+	 }elseif($current_page-2<0 && $current_page+2<=$total_page_post){
+	 	for($i=0;$i<=$current_page+2;$i++){
+			$page_pagination_post[]=$i;
+		}
+	 }elseif($current_page-2>=0 && $current_page+2>$total_page_post){
+	 	for($i=$current_page-2;$i<=$total_page_post;$i++){
+			$page_pagination_post[]=$i;
+		}
+	 }elseif($current_page-2<0 && $current_page+2>$total_page_post){
+	 	for($i=1;$i<=$total_page_post;$i++){
+			$page_pagination_post[]=$i;
+		}
+	 }
+	/*
+	 * Извлекаем название и текст темы.
+	 */
+	$sql_select_themes = 'SELECT * FROM themes WHERE id=:id';
+	$stm_select_themes = $dbh ->prepare($sql_select_themes);
+	$stm_select_themes -> execute(array('id' => $id));
+	$result_themes = $stm_select_themes -> fetch();
+	/*
+	 * Извлекаем ответы в теме
+	 */
+	$sql_select_post = 'SELECT * FROM posts WHERE themes_number=:id_theme LIMIT :id_start, :id_total';
+	$stm_select_post = $dbh ->prepare($sql_select_post);
+	$stm_select_post->bindValue(id_theme, $id, PDO::PARAM_STR);
+	$start = $current_page*$post_to_page - $post_to_page;
+	$stm_select_post->bindValue(id_start, $start, PDO::PARAM_INT);
+	$stm_select_post->bindValue(id_total, $post_to_page, PDO::PARAM_INT);
+	$stm_select_post -> execute();
+	$results_post = $stm_select_post -> fetchAll();
+
+	$dbh -> commit();
+	 /*
+	 * Формируем страницу выдачи
+	 */
+	?>
 <!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="utf-8">
+	<title>Тема №<?php echo $result_themes['id']; ?></title>
+	<link rel="stylesheet" href="bootstrap3/css/bootstrap.css">
+	<link rel="stylesheet" href="css/styles.css">
+	<link rel="stylesheet" href="css/admin.css">
+</head>
+<body>
+
+<div id="wrapper">
+	<h1>Тема №<?php echo $result_themes['id']; ?></h1>
+	<p>
+		<span class="subheader">Создана:</span>
+		<?php echo date("d.m.Y H:i:s", $result_themes['data_creation']);?>
+		<span class="subheader">Автор:</span>
+		<?php echo $result_themes['author']; ?>
+		<br />
+		<span class="subheader">Количество ответов:</span>
+		<?php echo $result_themes['number_replies']; ?>
+		<a href="index.php">Перейти на список тем.</a>
+	</p>
+	<p class="title"> <?php echo $result_themes['themes_title']; ?></p>
+
+	<div class="desc">
+
+		<?php echo $result_themes['themes_text']; ?>
+	</div>
+
+	<h2>Ответы</h2>
+ 	<?php
+	foreach ($results_post as $result){
+		echo "<div class='note'><p><span class='date'>".date("d.m.Y H:i:s",$result['data_post'])."</span><span class='name'>".$result['author']."</span></p><p>".$result['text']."</p></div>";
+	}
+	?>
+	<div>
+		<nav>
+			<ul class="pagination">
+			<?php
+			if($current_page != 1){
+					echo "<li class='disabled'><a href='?page=1'  aria-label='Previous'><span aria-hidden='true'>&laquo;</span></a></li>";
+				} 
+			foreach($page_pagination_post as $page_pagination){
+				if($page_pagination == $current_page){
+					$flag = "class='active'";
+				}else{
+					$flag='';
+				}
+				echo "<li ".$flag."><a href='?page=".$page_pagination."'>".$page_pagination."</a></li>"; 
+			}
+			echo "<li><a href='?page=".$total_page_post."' aria-label='Next'><span aria-hidden='true'>&raquo;</span></a></li>";
+			?>
+				
+			</ul>
+		</nav>
+
+	</div>
+	<?php
+	if(!empty($_GET['flag']) && (int)$_GET['flag'] == 1){
+		echo "<div class='info alert alert-info'>Запись успешно сохранена!</div>";
+	}
+	?>
+	<div id="form">
+				<form action="topic.php?topic=<?php echo $id; ?>&flag=1" method="POST">
+					<p><input class="form-control" name="author" placeholder="Ваше имя"></p>
+					
+					<p><textarea class="form-control" name="text" placeholder="Ваше сообщение"></textarea></p>
+					<p><input type="submit" class="btn btn-info btn-block" value="Сохранить"></p>
+				</form>
+			</div>
+		</div>
+
+	</body>
+</html>
+
+
+	<?php
+}catch(PDOException $e){
+	$dbh -> rollBack();
+	echo 'Ошибка базы данных: '.$e->getMessage();
+}
+?>
+<!--<!DOCTYPE html>
 <html lang="ru">
 	<head>
 		<meta charset="utf-8">  
@@ -122,7 +300,7 @@ echo time();
 			
 			-->
 			
-			<div id="form">
+			<!--<div id="form">
 				<form action="#form" method="POST">
 					<p><input class="form-control" placeholder="Ваше имя"></p>
 					
@@ -134,6 +312,6 @@ echo time();
 
 	</body>
 </html>
-
+-->
 
 			
