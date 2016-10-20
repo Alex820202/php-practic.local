74bc66ce88f2de79233b8216821e0823baacd757
Alex820202 <ads2004@bk.ru>
2016-10-20 15:26:20 +0700

    На проверку lesson_05 / 02-interview

--- lesson_05_interview_test/02_interview/check.php
+++ lesson_05_interview_test/02_interview/check.php
@@ -1,3 +1,52 @@
+<?php
+require_once(__DIR__.'/config.php');
+
+try{
+	$dbh = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
+	$dbh -> setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+	$dbh -> setAttribute( PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+	$dbh -> beginTransaction();
+	/*
+	* Выбираем из базы данных информацию о вопросе и ответах на него.
+	*/
+	$sql_select = 'SELECT * FROM Questions WHERE id=:id LIMIT 1';
+	$stm_select = $dbh -> prepare($sql_select);
+	$data['id'] = (int)$_GET['id'];
+	$stm_select -> execute($data);
+	$result = $stm_select -> fetch();
+	/*
+	* Обрабатываем пришедший ответ. 
+	*/
+	if(!empty($_POST)){
+		$sql_update = 'UPDATE Questions SET total_answer=:total_answer, int_answer_1=:int_answer_1, pct_answer_1=:pct_answer_1, int_answer_2=:int_answer_2, pct_answer_2=:pct_answer_2, int_answer_3=:int_answer_3, pct_answer_3=:pct_answer_3 WHERE id=:id';
+		$stm_update = $dbh -> prepare($sql_update);
+		$data['total_answer'] = $result['total_answer'] +1; //общее количество опрошенных
+		$stm_update -> bindValue(total_answer, $data['total_answer'], PDO::PARAM_INT);
+		/*
+		* Определяем количество ответивших на вопрос одним из трех вариантов ответа в людях и процентах.
+		*/
+		for($i=1; $i<=3; $i++){
+			$key_1 = 'int_answer_'.$i; 
+			$key_2 = 'pct_answer_'.$i;
+			if($i != (int)$_POST['radio']){
+				$data[$key_1] = $result[$key_1];
+				$data[$key_2] = round($data[$key_1]*100/$data['total_answer']);
+				$stm_update -> bindValue($key_1, $data[$key_1], PDO::PARAM_INT);
+				$stm_update -> bindValue($key_2, $data[$key_2], PDO::PARAM_INT);
+			}else{
+				$data[$key_1] = $result[$key_1]+1;
+				$data[$key_2] = round($data[$key_1]*100/$data['total_answer']);
+				$stm_update -> bindValue($key_1, $data[$key_1], PDO::PARAM_INT);
+				$stm_update -> bindValue($key_2, $data[$key_2], PDO::PARAM_INT);
+			}
+		}
+		$stm_update -> bindValue(id, (int)$_GET['id'], PDO::PARAM_INT);
+		$stm_update ->execute();
+	}
+	$dbh -> commit();
+	
+	?>
+	
 <!DOCTYPE html>
 <html lang="ru">
 	<head>
@@ -12,36 +61,25 @@
 		<div id="wrapper">
 			<h1>Результат опроса</h1>
 			<div class="info alert alert-info">
-				Общее количество опрошенных: <b>100</b>.
-				<br>
-				<b>1.</b> Ответили "до 20 лет": <b>40</b> человек, <b>40%</b> опрошенных.
-				<br>
-				<b>2.</b> Ответили "от 20 до 30": <b>28</b> человек, <b>28%</b> опрошенных.
-				<br>
-				<b>3.</b> Ответили "более 30": <b>32</b> человека, <b>32%</b> опрошенных.
+				Общее количество опрошенных: <b><?php echo $data['total_answer']; ?></b>.
+				<?php 
+				for($i=1; $i<=3; $i++){
+					$key_1 = 'answer_'.$i;
+					$key_2 = 'int_answer_'.$i;
+					$key_3 = 'pct_answer_'.$i;
+				echo "<br><b>1.</b>Ответили \"".$result[$key_1]."\": <b>".$data[$key_2]."</b> человек, <b> ".$data[$key_3]."%</b> опрошенных.<br>";
+				}
+				?>
 			</div>		
 			
 			<div class="note">
-			
-				<p class="answer">Ответ "до 20 лет":</p>
-				<div class="progress">
-					<div class="progress-bar" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 40%;">
-						40%
-					</div>
-				</div>
-				<p class="answer">Ответ "от 20 до 30":</p>
-				<div class="progress">
-					<div class="progress-bar" role="progressbar" aria-valuenow="2" aria-valuemin="0" aria-valuemax="100" style="width: 28%;">
-						28%
-					</div>
-				</div>
-				<p class="answer">Ответ "более 30":</p>
-				<div class="progress">
-					<div class="progress-bar" role="progressbar" aria-valuenow="2" aria-valuemin="0" aria-valuemax="100" style="width: 32%;">
-						32%
-					</div>
-				</div>
-				
+			<?php
+			for($i=1; $i<=3; $i++){
+					$key_1 = 'answer_'.$i;
+					$key_2 = 'pct_answer_'.$i;
+					echo "<p class='answer'>Ответ \"".$result[$key_1]."\":</p><div class='progress'><div class='progress-bar' role='progressbar' aria-valuenow='0' aria-valuemin='0' aria-valuemax='100' style='width:".$data[$key_2]."%;'>".$data[$key_2]."%</div></div>";
+				}
+				?>
 			</div>
 			
 			<div class="info alert alert-info">
@@ -52,5 +90,10 @@
 	</body>
 </html>
 
+<?php	
+} catch (PDOException $e) {
+	echo 'Ошибка базы данных: '.$e->getMessage();
+}
 
+?>
 			
--- lesson_05_interview_test/02_interview/index.php
+++ lesson_05_interview_test/02_interview/index.php
@@ -1,3 +1,16 @@
+<?php
+require_once(__DIR__.'/config.php');
+$id = 1; //номер вопроса из таблицы questions базы данных, в БД одна таблица, поэтому предполагаетя наличие до трех ответов на все вопросы.
+try{
+	$dbh = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
+	$dbh -> setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+	$dbh -> setAttribute( PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+	$sql = 'SELECT * FROM Questions WHERE id=:id LIMIT 1';
+	$stm = $dbh -> prepare($sql);
+	$stm -> execute(array('id'=>$id));
+	$result = $stm -> fetch();
+?>
+
 <!DOCTYPE html>
 <html lang="ru">
 	<head>
@@ -12,13 +25,13 @@
 		<div id="wrapper">
 			<h1>Опрос</h1>
 			<div class="info alert alert-info">
-				Укажите Ваш возраст:
+				<?php echo $result['question']; ?>:
 			</div>
-			<form action="check.php" method="POST">
+			<form action="check.php?id=<?php echo $id; ?>" method="POST">
 				<div class="note">
-					<p><input type="radio" name="radio" value="1"  id="r1"> <label for="r1">до 20 лет</label></p>
-					<p><input type="radio" name="radio" value="2"  id="r2"> <label for="r2">от 20 до 30</label></p>
-					<p><input type="radio" name="radio" value="3"  id="r3"> <label for="r3">более 30</label></p>
+					<p><input type="radio" name="radio" value="1"  id="r1"> <label for="r1"><?php echo $result['answer_1']; ?></label></p>
+					<p><input type="radio" name="radio" value="2"  id="r2"> <label for="r2"><?php echo $result['answer_2']; ?></label></p>
+					<p><input type="radio" name="radio" value="3"  id="r3"> <label for="r3"><?php echo $result['answer_3']; ?></label></p>
 				</div>
 				
 				<p><input type="submit" class="btn btn-success btn-block" value="Ответить"></p>
@@ -29,5 +42,10 @@
 	</body>
 </html>
 
+<?php	
+} catch (PDOException $e) {
+	echo 'Ошибка чтения из базы данных: '. $e->getMessage(); 
+}
+?>
 
 			
